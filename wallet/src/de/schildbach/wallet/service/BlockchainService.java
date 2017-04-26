104bf4148edb0e7fbf3e8b3a957df9a2ae6de227
abvhiael crowley <abvhiael@live.ca>
2017-04-26 14:32:02 -0300

    Conversion to GanjaCoin

--- wallet/src/de/schildbach/wallet/service/BlockchainService.java
+++ wallet/src/de/schildbach/wallet/service/BlockchainService.java
@@ -23,7 +23,7 @@ import javax.annotation.CheckForNull;
 
 import com.google.bitcoin.core.Peer;
 import com.google.bitcoin.core.StoredBlock;
-import mystphysx.solcoin.wallet.R;
+import abvhiael.ganjacoin.wallet.R;
 
 /**
  * @author Andreas Schildbach
