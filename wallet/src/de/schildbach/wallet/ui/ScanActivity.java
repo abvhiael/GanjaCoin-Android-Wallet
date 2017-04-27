c08271af3b5e3578d47df1bd2aa9b2580f155eb5
abvhiael crowley <abvhiael@live.ca>
2017-04-27 11:17:59 -0300

    Conversion to GanjaCoin

--- wallet/src/de/schildbach/wallet/ui/ScanActivity.java
+++ wallet/src/de/schildbach/wallet/ui/ScanActivity.java
@@ -58,7 +58,7 @@ import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
 
 import de.schildbach.wallet.camera.CameraManager;
-import mystphysx.solcoin.wallet.R;
+import abvhiael.ganjacoin.wallet.R;
 
 /**
  * @author Andreas Schildbach
