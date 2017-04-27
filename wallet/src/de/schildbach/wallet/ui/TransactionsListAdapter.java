43440461eb8769f725e43c64b1310b0897771daa
abvhiael crowley <abvhiael@live.ca>
2017-04-27 11:52:31 -0300

    Conversion to Ganjacoin

--- wallet/src/de/schildbach/wallet/ui/TransactionsListAdapter.java
+++ wallet/src/de/schildbach/wallet/ui/TransactionsListAdapter.java
@@ -51,7 +51,7 @@ import de.schildbach.wallet.AddressBookProvider;
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.util.CircularProgressView;
 import de.schildbach.wallet.util.WalletUtils;
-import mystphysx.solcoin.wallet.R;
+import abvhiael.ganjacoin.wallet.R;
 
 /**
  * @author Andreas Schildbach
